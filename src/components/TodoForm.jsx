b7432240d278ca077354f6b589f24ae110e1ead6
Wesley Silva <wesleysilva059@gmail.com>
2024-08-25 16:40:58 -0300

    feat: Implementações adicionais todo

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { useState } from 'react'
 
-const TodoForm = () => {
+const TodoForm = ( {addTodo} ) => {
+    const [value, setValue] = useState("");
+    const [category, setCategory] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!value || !category) return
+        addTodo(value, category);
+        setValue("");
+        setCategory("");
+    };
+
   return <div className="todo-form">
     <h2>Criar Tarefa</h2>
-    <form>
-        <input type="text" placeholder="Digit o titulo" />
-        <select>
+    <form onSubmit={handleSubmit}>
+        <input 
+            type="text"
+            placeholder="Digit o titulo"
+            value={value}
+            onChange={(e) => setValue(e.target.value)} />
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
             <option value="">Selecione um categoria</option>
             <option value="Trabalho">Trabalho</option>
             <option value="Pessoal">Pessoal</option>
